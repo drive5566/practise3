77132e5c7df949a461965063f39ff460f67e3a47
Eddie Kao <eddie@5xruby.tw>
2023-05-11 13:24:49 +0800

    增加互動_2

--- js/dom.js
+++ js/dom.js
@@ -14,17 +14,60 @@ for(i = 0 ; i < li.length ; i++){
 
 var dt = document.querySelector('.dt')
 var dd = document.querySelector('.dd')
+var phone = document.querySelector('.phone')
 
 
+let a
 dt.addEventListener('mouseover',function(){
     dd.style.display ='block'
+    clearInterval(a)
+    a = setInterval(shin,500)
+
 })
+function shin (){
+    if(phone.style.color == 'black'){
+        phone.style.color = 'white'
+    }else{
+        phone.style.color = 'black'
+    }
+}
 
 
 
-var a = document.querySelector('.hotwrods li')
-var lili = a.children
+    // 1獲取元素
+    var side = document.querySelector('.side')
+    var boxhd = document.querySelector('.box_hd')
+    var main = document.querySelector('.main')
+    var sidetop = side.offsetTop - main.offsetTop
 
+
+    // 2頁面滾動事件 scroll
+    document.addEventListener('scroll',function(){
+        // console.log(11);
+        // window.pageYoffset 頁面被捲去的頭部
+        // console.log(window.pageYOffset); 
+
+        if(window.pageYOffset >= boxhd.offsetTop){
+            side.style.display=('none')
+        }else{
+            side.style.display=('block')
+
+        }
+
+        if(window.pageYOffset >= main.offsetTop){
+            side.style.position = ('fixed')
+            side.style.top = sidetop + 'px'
+        }else{
+            side.style.position = ('absolute')
+           
+            side.style.top = '250px' 
+
+        }
+
+    })
+
+
+    
 
 
 
